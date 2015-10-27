f67a3af0dacc0dfc1e40cad48c48e304f4e721b2
masterUNG <phrombutr@gmail.com>
2015-10-27 10:15:05 +0700

    Project Start

---
+++ app/src/main/java/appewtc/masterung/hellodji3/DJIAoaActivity.java
@@ -0,0 +1,13 @@
+package appewtc.masterung.hellodji3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DJIAoaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_djiaoa);
+    }
+}
